ca392d093c05a43f06b54978314fe8fe1918dbf0
MicaiahReid <micaiahreid@gmail.com>
2023-05-24 18:28:36 -0400

    default code snippet

---
+++ public/snippets/default.js
@@ -0,0 +1,7 @@
+/**
+ * Hi! Welcome to the Stacks.js REPL! All stacks.js dependencies are loaded into
+ * this console and ready for you to use! Any data you `console.log` will appear
+ * below in the console logs. Write some code and click the Run button to get started!
+ */
+
+console.log("Hello, world!");
