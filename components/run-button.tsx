5d5588219e083bfac50ef32dbfb52b2a874f6780
MicaiahReid <micaiahreid@gmail.com>
2023-05-25 15:56:12 -0400

    reset console after every run of code

--- components/run-button.tsx
+++ components/run-button.tsx
@@ -69,6 +69,7 @@ const RunButton = () => {
             }
         };
         ${code}
+        console = oldConsole;
     `;
     script.innerHTML = consoleCode;
     script.setAttribute("type", "module");
